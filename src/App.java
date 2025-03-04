8445b6276277b6da52c314c49116661e715458ee
jordblank <26jblankstein@gannacademy.org>
2025-03-04 12:25:54 -0500

    uhyuji

--- src/App.java
+++ src/App.java
@@ -4,4 +4,23 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+    public void settings(){
+        size(400, 400);
+        }
+    
+    public void setup(){
+        background (145, 143, 255);
+    }
+    public void draw (){
+        stroke (13, 11, 125);
+        strokeWeight(5);
+        rect(50,50,200,150);
+        fill(116, 114, 196);
+        stroke (42, 31, 100);
+        strokeWeight(3);
+        ellipse(300, 300, 100, 100);
+        fill(93, 91, 186);
+        line(250, 200, 250, 290);
+    }
+
 }
